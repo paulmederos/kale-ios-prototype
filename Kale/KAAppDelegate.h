723563a9d285fde5a79d27d1eb88de5eb3c8d99b
Paul Mederos <pvm@enchant.co>
2012-11-20 18:05:36 -0500

    Initial Commit

---
+++ Kale/KAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KAAppDelegate.h
+//  Kale
+//
+//  Created by Paul Mederos Jr on 11/20/12.
+//  Copyright (c) 2012 Enchant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Kale/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Kale
+//
+//  Created by Paul Mederos Jr on 11/20/12.
+//  Copyright (c) 2012 Enchant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KAAppDelegate class]));
+    }
+}
