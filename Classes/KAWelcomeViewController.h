c1318c6ea185aeddd9df4663297160b8d7004d11
Paul Mederos <paulmederos@me.com>
2013-02-22 12:09:26 -0500

    Preparing new UI + welcome view

---
+++ Classes/KAWelcomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  KAWelcomeViewController.h
+//  Kale
+//
+//  Created by Paul Mederos on 2/22/13.
+//  Copyright (c) 2013 Enchant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KAWelcomeViewController : UIViewController <UIWebViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *outputLabel;
+
+@end
---
+++ Classes/KAWelcomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  KAWelcomeViewController.m
+//  Kale
+//
+//  Created by Paul Mederos on 2/22/13.
+//  Copyright (c) 2013 Enchant. All rights reserved.
+//
+
+#import "KAWelcomeViewController.h"
+
+@interface KAWelcomeViewController ()
+
+@end
+
+@implementation KAWelcomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
